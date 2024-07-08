5aac82f111dd7ae3031dfdb71a7c9c59846dd0c6
Adnan Ansari <138770731+26Adnanansari@users.noreply.github.com>
2024-07-08 19:24:23 +0500

    test2

--- app/main.py
+++ app/main.py
@@ -6,4 +6,7 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "World1", "World2": "Pakistan zinda bad 1234567 80"}
+@app.get("/about")
+def read_root():
+    return {"Institue": "PIAIC", "Institue": "GIAIC"}
 
